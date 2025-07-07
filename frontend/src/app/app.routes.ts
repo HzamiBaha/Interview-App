70165a864a08ecdf92409f049ef1bf088da311cc
kacem <kacemmeherzi52@gmail.com>
2025-07-07 11:51:02 +0100

    tasks details added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,21 @@ import { LoginComponent } from './auth/login/login.component';
 import { authGuard } from './auth/auth.guard';
 import { RegisterComponent } from './auth/register/register.component';
 import { TaskManagementComponent } from './tasks/task-management/task-management.component';
+import { TaskDetailsComponent } from './tasks/task-details/task-details.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'tasks', component: TaskManagementComponent, canActivate: [authGuard] },
+  {
+    path: 'tasks',
+    component: TaskManagementComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'details/:id',
+    component: TaskDetailsComponent,
+    canActivate: [authGuard],
+  },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' }
+  { path: '**', redirectTo: '/login' },
 ];
