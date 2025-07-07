d8adbc557e7899c071c906efb105ee678d3ff69b
kacem <kacemmeherzi52@gmail.com>
2025-07-07 11:51:49 +0100

    auth interceptor added

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -3,8 +3,18 @@ import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-// Inject the AuthService to access the current user token
-// set the Authorization header with the token if the user is authenticated
-
+  // Inject the AuthService to access the current user token
+  // set the Authorization header with the token if the user is authenticated
+  const authService = inject(AuthService);
+  const token = authService.currentUser$.subscribe((token) => {
+    if (token && authService.isAuthenticated()) {
+      const token = localStorage.getItem('token');
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+  });
   return next(req);
 };
