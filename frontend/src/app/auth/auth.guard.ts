fa56b0d760f6f05af50f78cbc131663bb5a4d8f0
kacem <kacemmeherzi52@gmail.com>
2025-07-07 11:51:14 +0100

    auth guard added

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -3,9 +3,14 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 export const authGuard: CanActivateFn = () => {
-
   // Inject AuthService and Router using Angular's dependency injection
   // navigate to /login if user is not authenticated
-
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (authService.isAuthenticated()) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+  }
   return false;
 };
